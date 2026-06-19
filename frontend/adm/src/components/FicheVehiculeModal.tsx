e0ea63838c33c4ca1fc1bee78fe95e6109ad6f30
AudreyL59 <a.loudieux@exosphere.fr>
2026-06-19 11:47:57 +0200

    fix(fiche-vehicule): vire import AnimatePresence inutilise (TS6133)

--- frontend/adm/src/components/FicheVehiculeModal.tsx
+++ frontend/adm/src/components/FicheVehiculeModal.tsx
@@ -7,7 +7,7 @@
  */
 
 import { useEffect, useState } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 import {
   Car as CarIcon,
   ExternalLink,
